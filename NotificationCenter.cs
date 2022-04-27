f6ba37c3b21000b0dffc7713792395687c7273ba
Ryan Boyer <ryanjboyer@gmail.com>
2022-04-27 00:43:02 -0600

    Initial commit

---
+++ NotificationCenter.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NotificationCenter {
+    public class NotificationCenter : MonoBehaviour {
+        private static NotificationCenter shared = null;
+
+        public delegate void Callback(Notification notification);
+
+        private static Dictionary<int, NotificationEvent> events = new Dictionary<int, NotificationEvent>();
+        private static Dictionary<int, List<Callback>> observers = new Dictionary<int, List<Callback>>();
+
+        private class NotificationEvent {
+            public event Callback eventDelegate;
+
+            public void Invoke(Notification notification) {
+                eventDelegate?.Invoke(notification);
+            }
+        }
+
+        private void Awake() {
+            shared = this;
+        }
+
+        private void OnApplicationQuit() {
+            foreach (int n in observers.Keys) {
+                observers[n].Clear();
+            }
+        }
+
+        #region Static
+
+        public static void Post(Notification.Name notification, object data) {
+            int hash = notification.GetHashCode();
+            ValidateNotification(hash);
+            Notification obj = new Notification(notification, data);
+            events[hash].Invoke(obj);
+        }
+
+        public static void AddObserver(Notification.Name notification, Callback action) {
+            int hash = notification.GetHashCode();
+            ValidateNotification(hash);
+
+            observers[hash].Add(action);
+            events[hash].eventDelegate += action;
+        }
+
+        public static void RemoveObserver(Notification.Name notification, Callback action) {
+            int hash = notification.GetHashCode();
+            ValidateNotification(hash);
+
+            observers[hash].Remove(action);
+            events[hash].eventDelegate -= action;
+        }
+
+        #endregion
+
+        #region Internal
+
+        private static void ValidateNotification(int notification) {
+            if (!events.ContainsKey(notification)) {
+                events.Add(notification, new NotificationEvent());
+                observers.Add(notification, new List<Callback>());
+            }
+        }
+
+        #endregion
+    }
+
+    public class Notification : EventArgs {
+        public readonly Name name;
+        public readonly object data;
+
+        public Notification(Name name, object data) {
+            this.name = name;
+            this.data = data;
+        }
+
+        public class Name {
+            public readonly string value;
+
+            public Name(string value) {
+                this.value = value;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Some Package
-Some package description
+# NotificationCenter
+An event system for Unity + C# based on Apple's [NotificationCenter](https://developer.apple.com/documentation/foundation/notificationcenter).
+
+## Installation
+**Recommended Installation** (Unity Package Manager)
+- "Add package from git URL..."
+- `https://github.com/ryanslikesocool/NotificationCenter.git`
+
+**Alternate Installation** (not recommended)
+- Get the latest [release](https://github.com/ryanslikesocool/NotificationCenter/releases)
+- Import into your project's Plugins folder
+
+## Usage
+Add the NotificationCenter component to an object in your scene.  Your scene and the NotificationCenter's lifecycles are tied together.\
+
+Notifications can take in any object and send it to observers.  Just cast it to the desired type on the recieving end.\
+Notification objects are optional.  If you don't need to attach an object, just pass in `null`.
+
+```cs
+//// to post an event
+
+// the notification name can be stored somewhere so a new string isn't always created
+Notification.Name notificationID = new Notification.Name("some identifying string");
+int anyObject = 42;
+
+NotificationCenter.Post(notificationID, anyObject);
+
+
+//// and to recieve
+
+// add an observer with a Notification.Name and a handler
+NotificationCenter.AddObserver(notificationID, MyNotificationHandler);
+
+// remove the observer when it doesn't need to recieve any more events
+NotificationCenter.RemoveObserver(notificationID, MyNotificationHandler);
+
+public void MyNotificationHandler(Notification notification) {
+	// get the notification name, in case you need to check for the sender
+	Notification.Name notificationID = notification.name;
+	
+	// get the object, if desired
+	int notificationObject = (int)notification.data; // 42!
+	
+	// etc...
+}
+```
