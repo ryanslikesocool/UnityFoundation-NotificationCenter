c8dc30ffec9a04cd47742342a9375f2c55016ed9
Ryan Boyer <ryanjboyer@gmail.com>
2023-12-23 01:28:58 -0700

    Improved notification field unwrapping
    - Now throwing errors on failed force unwraps
    - Added `TryReadSender<T>(out T _)` and `ReadSender<T>()`
    - Documentation tweaks

--- Runtime/Notification.cs
+++ Runtime/Notification.cs
@@ -30,18 +30,55 @@ namespace Foundation {
 			this.data = data;
 		}
 
+		// MARK: - Sender
+
+		/// <summary>
+		/// Forcefully unwrap the notification sender as the given type.
+		/// </summary>
+		/// <typeparam name="T">The expected type of the notification sender.</typeparam>
+		/// <exception cref="NotificationCenter.UnexpectedSender">When the notification sender is not of the given type.</exception>
+		public T ReadSender<T>() {
+			if (sender is not T obj) {
+				throw new NotificationCenter.UnexpectedSender(this, typeof(T));
+			}
+			return obj;
+		}
+
+		/// <summary>
+		/// Attempt to unwrap the notification sender as the given type.
+		/// </summary>
+		/// <typeparam name="T">The expected type of the notification sender.</typeparam>
+		/// <param name="result">The result of unwrapping the notification sender; <see langword="default"/> if the sender was not of the expected type.</param>
+		/// <returns><see langword="true"/> if the notification sender was unwrapped; <see langword="false"/> otherwise.</returns>
+		public bool TryReadSender<T>(out T result) {
+			if (sender is T _result) {
+				result = _result;
+				return true;
+			} else {
+				result = default;
+				return false;
+			}
+		}
+
 		// MARK: - Data
 
 		/// <summary>
 		/// Forcefully unwrap the notification data as the given type.
 		/// </summary>
-		public T ReadData<T>() => (T)data;
+		/// <typeparam name="T">The expected type of the notification data.</typeparam>
+		/// <exception cref="NotificationCenter.UnexpectedData">When the notification data is not of the given type.</exception>
+		public T ReadData<T>() {
+			if (data is not T obj) {
+				throw new NotificationCenter.UnexpectedData(this, typeof(T));
+			}
+			return obj;
+		}
 
 		/// <summary>
 		/// Attempt to unwrap the notification data as the given type.
 		/// </summary>
-		/// <typeparam name="T">The type to read the notification data as.</typeparam>
-		/// <param name="result"></param>
+		/// <typeparam name="T">The expected type of the notification data.</typeparam>
+		/// <param name="result">The result of unwrapping the notification data; <see langword="default"/> if the data was not of the expected type.</param>
 		/// <returns><see langword="true"/> if the notification data was unwrapped; <see langword="false"/> otherwise.</returns>
 		public bool TryReadData<T>(out T result) {
 			if (data is T _result) {
